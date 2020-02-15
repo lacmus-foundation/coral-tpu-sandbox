433a4cdeaed414ce8ccbc41ec92af96a2e984a7b
gosha20777 <gosha20777@live.ru>
2020-02-15 23:32:30 +0300

    fix nimbers

--- src/research/slim/datasets/ladd.py
+++ src/research/slim/datasets/ladd.py
@@ -31,7 +31,7 @@ slim = tf.contrib.slim
 
 _FILE_PATTERN = 'ladd_%s_*.tfrecord'
 
-SPLITS_TO_SIZES = {'train': 3320, 'validation': 350}
+SPLITS_TO_SIZES = {'train': 2485, 'validation': 270}
 
 _NUM_CLASSES = 2
 
